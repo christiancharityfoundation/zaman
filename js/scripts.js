a75f9c06e4e389b678ad5588e5153c2b8c18710f
christiancharityfoundation <47373580+christiancharityfoundation@users.noreply.github.com>
2019-02-06 02:01:17 +0000

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+    $("#wwd-btn").click(function(){
+      $("#aboutus, #howwework, #wherewework, #getinvolved").hide();
+      $("#whatwedo").animate({opacity: 1}, 1000)
+      $("#whatwedo").show();
+
+    });
